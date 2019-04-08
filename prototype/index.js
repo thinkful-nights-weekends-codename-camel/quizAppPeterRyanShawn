f07c04f337c2134fb880e916b4de908332e82051
Peter Tardif <peter.tardif@gmail.com>
2019-04-07 23:40:19 -0600

    cleaned up console logs

--- prototype/index.js
+++ prototype/index.js
@@ -9,7 +9,7 @@ function showQuestion() {
     let questionCount = STORE.questions.length;
     let questionNumber = STORE.currentQuestion;
 
-    if (questionNumber < 2) { // questionCount - 1) 
+    if (questionNumber < questionCount - 1) {
         let question = STORE.questions[questionNumber].question;
         let options = STORE.questions[questionNumber].options;
 
@@ -50,8 +50,6 @@ function checkAnswer() {
         let chosenAnswer = $('input:checked');
         let value = parseInt(chosenAnswer.val());
         let questionNumber = STORE.currentQuestion;
-
-        console.log(chosenAnswer);
         let correctAnswer = STORE.questions[questionNumber].correct;
 
         if (value === correctAnswer) {
@@ -104,13 +102,11 @@ function showAnswerWrong() {
 
 // Show the final page with results
 function showFinalPage(rating) {
-    console.log("yoyoyo");
     let score = STORE.currentScore;
     let questionCount = STORE.questions.length;
 
     switch (rating) {
         case 0:
-            console.log("Hello from space");
             // show mediocre results view
             $('.generate-questions').html( `
             <section role="region">
@@ -146,7 +142,6 @@ function showFinalPage(rating) {
 }
 
 function startNewQuiz() {
-    console.log("new console log");
     $('.generate-questions').on('click', '#restart-quiz', event => {
         location.reload();
     })
@@ -156,7 +151,6 @@ function startNewQuiz() {
 // Assess user's score
 function assessScore() {
     let score = STORE.currentScore;
-    console.log(score);
     if (score < 4) {
         showFinalPage(0);
     } else if (score <= 8 && score >= 4) {
